d934a863b8aec6e6110ca584848b401f4f763154
TCG <ssibonokuhle@gmail.com>
2021-01-21 15:57:47 +0200

    added props

---
+++ timer-app/src/App.js
@@ -0,0 +1,37 @@
+ import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class Timer extends React.Component{
+  constructor(props){
+      super (props)
+      this.state = {
+          count : 0
+
+      }
+
+
+      this.startTimer = this.startTimer.bind(this)
+  }
+startTimer() {
+  this.timer = setInterval (() =>
+  this.setState({
+      count: this.state.count + 1 
+  }), 1000
+  )
+}
+  render() {
+      return(
+          <div className = "container">
+              <button onclick = {this.start}>Start</button>
+              <h1>Timer</h1>
+          </div>
+
+          
+      );
+  }
+
+}
+export default Timer;
+
---
+++ timer-app/src/timer.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Timer extends React.Component{
+    constructor(props){
+        super (props)
+        this.state = {
+            count : 0
+
+        }
+        this.startTimer = this.startTimer.bind(this)
+    }
+startTimer() {
+    this.timer = setInterval (() =>
+    this.setState({
+        count: this.state.count + 1 
+    }), 1000
+    )
+}
+    render() {
+        return(
+            <div className = "container">
+                <button>Start</button>
+                <h1>Timer</h1>
+            </div>
+        );
+    }
+
+}
+export default Timer;
