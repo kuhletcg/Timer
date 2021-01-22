f4a9825f8c13c7a52ff99e59163383436f2f7fef
TCG <ssibonokuhle@gmail.com>
2021-01-22 10:29:40 +0200

    added state in line 14

--- timer-app/src/App.js
+++ timer-app/src/App.js
@@ -1,37 +1,50 @@
- import React from 'react';
+import React, { Component } from 'react'
 
-import logo from './logo.svg';
-import './App.css';
+export default class Timer extends Component {
+    state = {
+        minutes: 1,
+        seconds: 0,
+    }
 
-class Timer extends React.Component{
-  constructor(props){
-      super (props)
-      this.state = {
-          count : 0
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state
 
-      }
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            } 
+        }, 1000)
+    }
 
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
 
-      this.startTimer = this.startTimer.bind(this)
-  }
-startTimer() {
-  this.timer = setInterval (() =>
-  this.setState({
-      count: this.state.count + 1 
-  }), 1000
-  )
-}
-  render() {
-      return(
-          <div className = "container">
-              <button onclick = {this.start}>Start</button>
-              <h1>Timer</h1>
-          </div>
-
+    render() {
+        const { minutes, seconds } = this.state
+        return (
+            <div>
+            
+                { minutes === 0 && seconds === 0
+                    ? <h1>Kuhle</h1>
+                    : <h1>start {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                   
+                }
+                
+            </div>
           
-      );
-  }
-
+        )
+    }
 }
-export default Timer;
-
